0aa229781feacc1c4f049feac2fed8ee684c0b99
cybranker <cybranker@yandex.ru>
2020-10-08 15:05:11 +0300

    Добавляет маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import MainScreen from "../main-screen/main-screen";
+import OfferScreen from "../offer-screen/offer-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import AuthScreen from "../auth-screen/auth-screen";
 
 
 const App = (props) => {
   const {numberPlaces} = props;
 
   return (
-    <MainScreen numberPlaces={numberPlaces} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen numberPlaces={numberPlaces} />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/offer/:id">
+          <OfferScreen />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
