f8c3798e39fbd96119484beaa2830e67a20a883d
cybranker <cybranker@yandex.ru>
2020-10-24 14:47:02 +0300

    Добавляет компонент 'Карта'

---
+++ src/components/map/map.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import leaflet from "leaflet";
+
+import "../../../node_modules/leaflet/dist/leaflet.css";
+
+const Map = () => {
+  return (
+    <div id="map"></div>
+  );
+};
+
+export default Map;
