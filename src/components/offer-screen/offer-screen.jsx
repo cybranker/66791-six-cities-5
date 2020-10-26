6c58d06c4d6dcd32a36bcaea4d823f957a713253
cybranker <cybranker@yandex.ru>
2020-10-26 18:54:59 +0200

    'Список отзывов' и 'Отзыв' вынесены в отдельные компоненты

--- src/components/offer-screen/offer-screen.jsx
+++ src/components/offer-screen/offer-screen.jsx
@@ -2,6 +2,7 @@ import React, {PureComponent} from "react";
 import PropTypes from 'prop-types';
 import {Link, Redirect} from 'react-router-dom';
 import {OfferType} from "../../const";
+import ReviewsList from "../reviews-list/reviews-list";
 import ReviewForm from "../review-form/review-form";
 
 class OfferScreen extends PureComponent {
@@ -138,30 +139,7 @@ class OfferScreen extends PureComponent {
                 </div>
                 <section className="property__reviews reviews">
                   <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
-                  <ul className="reviews__list">
-                    {reviews.map((review, i) => (
-                      <li key={`review-${i}}`} className="reviews__item">
-                        <div className="reviews__user user">
-                          <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                            <img className="reviews__avatar user__avatar" src={review.picture} width="54" height="54" alt="Reviews avatar"/>
-                          </div>
-                          <span className="reviews__user-name">{review.name}</span>
-                        </div>
-                        <div className="reviews__info">
-                          <div className="reviews__rating rating">
-                            <div className="reviews__stars rating__stars">
-                              <span style={{width: (20 * Math.round(review.rating)) + `%`}}></span>
-                              <span className="visually-hidden">Rating</span>
-                            </div>
-                          </div>
-                          <p className="reviews__text">
-                            {review.comment}
-                          </p>
-                          <time className="reviews__time" dateTime="2019-04-24">{review.date.toLocaleDateString(`en-US`, {year: `numeric`, month: `long`})}</time>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
+                  <ReviewsList reviews={reviews} />
                   <ReviewForm/>
                 </section>
               </div>
