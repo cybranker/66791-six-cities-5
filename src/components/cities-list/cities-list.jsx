6f8a091cfe290d4a4e3d36104aaa9d76ad0d7d14
cybranker <cybranker@yandex.ru>
2020-11-17 16:42:15 +0200

    Добавляет моки городов и отрисовывает на главной странице

--- src/components/cities-list/cities-list.jsx
+++ src/components/cities-list/cities-list.jsx
@@ -1,19 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
 import CitiesItem from "../cities-item/cities-item";
+import cities from "../../mocks/cities";
 
-import mainScreenProp from "../main-screen/main-screen.prop";
-
-const CitiesList = ({currentCity, offers, changeCity, getListOffers}) => {
-  let cities = {};
-
-  offers.forEach((offer) => {
-    cities[offer.city.name] = offer.city;
-  });
-
-  cities = Object.values(cities);
-
+const CitiesList = ({currentCity, changeCity, getListOffers}) => {
   return (
     <ul className="locations__list tabs__list">
       {cities.map((city, i) => (
@@ -32,13 +22,8 @@ CitiesList.propTypes = {
     }).isRequired,
     name: PropTypes.string.isRequired
   }).isRequired,
-  offers: mainScreenProp,
   changeCity: PropTypes.func.isRequired,
   getListOffers: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state) => ({
-  offers: state.offers
-});
-
-export default connect(mapStateToProps)(CitiesList);
+export default CitiesList;
