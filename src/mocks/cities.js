a9f401dc8d34fc16a322f1543da986e5941aaa80
cybranker <cybranker@yandex.ru>
2020-11-23 10:42:27 +0200

    Исправляет координаты городов в моках

--- src/mocks/cities.js
+++ src/mocks/cities.js
@@ -1,42 +1,42 @@
 export default [{
   location: {
-    lat: 48.85341,
-    lon: 2.3488,
+    lat: 48.85661,
+    lon: 2.351499,
     zoom: 10
   },
   name: `Paris`
 }, {
   location: {
-    lat: 50.93333,
-    lon: 6.95,
+    lat: 50.938361,
+    lon: 6.959974,
     zoom: 10
   },
   name: `Cologne`
 }, {
   location: {
-    lat: 50.85045,
-    lon: 4.34878,
+    lat: 50.846557,
+    lon: 4.351697,
     zoom: 10
   },
   name: `Brussels`
 }, {
   location: {
-    lat: 52.370216,
-    lon: 4.895168,
+    lat: 52.37454,
+    lon: 4.897976,
     zoom: 10
   },
   name: `Amsterdam`
 }, {
   location: {
-    lat: 53.54948738572969,
-    lon: 9.998931884765625,
+    lat: 53.550341,
+    lon: 10.000654,
     zoom: 10
   },
   name: `Hamburg`
 }, {
   location: {
-    lat: 51.22516285803612,
-    lon: 6.7753028869628915,
+    lat: 51.225402,
+    lon: 6.776314,
     zoom: 10
   },
   name: `Dusseldorf`
